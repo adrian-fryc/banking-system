4a60ccf385d6fe11443b997d58a49e57b764edb1
Adrian Fryc <adrian.fryc@gmail.com>
2026-04-07 13:35:31 +0200

    feat: add finding transaction with longest currency name

--- src/main/java/pl/mentor/banking/BankReportService.java
+++ src/main/java/pl/mentor/banking/BankReportService.java
@@ -1,7 +1,9 @@
 package pl.mentor.banking;
 
 import java.math.BigDecimal;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 
 public class BankReportService {
     public BigDecimal sumTransaction(List<Transaction> transactions, String targetCurrency){
@@ -14,5 +16,12 @@ public class BankReportService {
                 ;
 
         return sumTr;
+    }
+
+    public Optional<Transaction> getLongestCurrencyNameTransaction(List<Transaction> transactions){
+
+        return transactions.stream()
+                .max(Comparator.comparingInt(t -> t.currency().length()))
+                ;
     }
 }
